b13d62b14bf3a71e433764163474ea8155526616
dhanushiya06 <dhanushiya510@gmail.com>
2022-09-26 17:44:15 +0530

    initial commit

--- src/main/java/WaveFormArray.java
+++ src/main/java/WaveFormArray.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 public class WaveFormArray {
     int temp = 0;
-    public void swapingOfTwoAdjacentNumbers(int[] array, int first, int second) {
+    public void swappingOfTwoAdjacentNumbers(int[] array, int first, int second) {
         temp = array[first];
         array[first] = array[second];
         array[second] = temp;
     }
 
     public void sortingArrayInWaveForm(int[] arr, int length) {
-        int i;
-        for ( i = 0; i < arr[i + 1] && i % 2 == 1;i++){
-            swapingOfTwoAdjacentNumbers(arr, i, i + 1);
+       int i;
+        for (i = 0; i < arr[i + 1] && i % 2 == 1;i++){
+            swappingOfTwoAdjacentNumbers(arr, i, i + 1);
         }
         if ((arr[i] < arr[i + 1] && i % 2 == 0)) {
-            swapingOfTwoAdjacentNumbers(arr, i, i + 1);
+            swappingOfTwoAdjacentNumbers(arr, i, i + 1);
         }
         System.out.println("array in waveform is:");
         System.out.println(Arrays.toString(arr));
@@ -33,6 +33,7 @@ public class WaveFormArray {
         }
         System.out.println("the array before sorting it in waveform is:");
         System.out.println(Arrays.toString(array));
+
         WaveFormArray wave = new WaveFormArray();
         wave.sortingArrayInWaveForm(array, lengthOfTheArray);
     }
