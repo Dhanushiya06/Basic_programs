447ee273d67932d305c44d21fb30bd5c7400bfb6
dhanushiya06 <dhanushiya510@gmail.com>
2022-10-10 14:43:42 +0530

    initial commit

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -1,31 +1,48 @@
 import java.util.Scanner;
-public class Prime {
-    public static boolean isPrime(int n) {
-        if (n == 1 || n == 0) {
-            return false;
-        }
-        if (n == 2) {
-            return true;
-        }
-        for (int i = 2; i <= n /2 ; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("a :");
-        int a = sc.nextInt();
-        System.out.print("b :");
-        int b = sc.nextInt();
 
-        for (int i = a; i <= b; i++) {
-            if (isPrime(i)) {
-                System.out.println(i + " ");
+public class Prime {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+//        Print prime numbers within a range
+
+        System.out.println("Enter min: ");
+        int min = scanner.nextInt();
+        System.out.println("Enter max: ");
+        int max = scanner.nextInt();
+        int flag = 0;
+        for (int n = min; n <= max; n++) {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    flag = flag + 1;
+                    break;
+                }
             }
+            if (flag == 0 && n != 1) {
+                System.out.print(n + " ");
+            } else {
+                flag = 0;
+            }
+        }
+
+
+        //Any number is Check prime or not
+        System.out.println("Enter the number: ");
+        int m = scanner.nextInt();
+        for (int i = 2; i <= m / 2; i++) {
+            if (m % i == 0) {
+                flag = flag + 1;
+                break;
+            }
+        }
+        if (flag == 0) {
+            System.out.println(m + " Prime number");
+        } else {
+            System.out.println(m + " Not Prime number");
         }
     }
 }
+
+
+
 
